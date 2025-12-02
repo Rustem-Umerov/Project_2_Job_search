316d93335959e7eb5644065f7959474cc46ec888
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-12-02 16:44:03 +0300

    Added an UnexpectedStatusError exception to the exceptions.py module - thrown if the validation function returns an unexpected status.

--- job_search/utils/exceptions.py
+++ job_search/utils/exceptions.py
@@ -2,3 +2,9 @@ class FilterCancelled(Exception):
     """Исключение, выбрасываемое при отмене фильтра пользователем."""
 
     pass
+
+
+class UnexpectedStatusError(Exception):
+    """Исключение, выбрасываемое, если валидирующая функция вернула неожиданный статус."""
+
+    pass
