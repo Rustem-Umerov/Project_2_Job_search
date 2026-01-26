f87f1e4def54e90a4ce55d6d95cfb29b39741383
Rustem Umerov <rustem.umerov.00@yandex.ru>
2026-01-26 16:18:39 +0300

    In the base_api.py module, I added the get_all_vacancies method to the VacancyAPI(ABC) class. This method retrieves all vacancies by keyword and, by default, allows additional parameters to be added.

--- job_search/api/base_api.py
+++ job_search/api/base_api.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+from job_search.api.types import VacancyResult
+
 
 class VacancyAPI(ABC):
     """
@@ -9,10 +11,25 @@ class VacancyAPI(ABC):
     """
 
     @abstractmethod
-    def get_vacancies(self, keyword: str, **kwargs: Any) -> list[dict[str, Any]]:
+    def get_vacancies(self, keyword: str, **kwargs: Any) -> VacancyResult:
         """
-        Возвращает список вакансий по ключевому слову.
-        params могут содержать фильтры (регион, количество на страницу и т.д.).
+        Возвращает список вакансий по ключевому слову и, по умолчанию, можно добавить дополнительные параметры.
+
+        :param keyword: Ключевое слово
+        :param kwargs: Словарь с дополнительными фильтрами (регион, количество на страницу и т.д.)
+        :return: VacancyResult: объект Pydantic‑модели с результатами поиска.
+        """
+
+        pass
+
+    @abstractmethod
+    def get_all_vacancies(self, keyword: str, **kwargs: Any) -> VacancyResult:
+        """
+        Метод получает все вакансии по ключевому слову и, по умолчанию, можно добавить дополнительные параметры.
+
+        :param keyword: Ключевое слово для поиска вакансий.
+        :param kwargs: Дополнительные параметры запроса (например, регион, опыт).
+        :return: VacancyResult: объект Pydantic‑модели с результатами поиска.
         """
 
         pass
